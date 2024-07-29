49b8a847890430725fd7bc6c77314829af4739ac
LieryangStack <sxlisibo@gmail.com>
2024-07-29 21:57:44 +0800

    update

--- gio/glistmodel.h
+++ gio/glistmodel.h
@@ -40,29 +40,34 @@ G_DECLARE_INTERFACE(GListModel, g_list_model, G, LIST_MODEL, GObject)
 struct _GListModelInterface
 {
   GTypeInterface g_iface;
-
+  /* 接口虚函数：得到列表里面每个item的类型 */
   GType     (* get_item_type)   (GListModel *list);
-
+  /* 接口虚函数：得到列表里面有多少个元素item */
   guint     (* get_n_items)     (GListModel *list);
-
+  /* 接口虚函数：得到@position位置的item（也就是获取到数组位置的内容item） */
   gpointer  (* get_item)        (GListModel *list,
                                  guint       position);
 };
 
+/* 得到列表里面每个item的类型 */
 GIO_AVAILABLE_IN_2_44
 GType                   g_list_model_get_item_type                      (GListModel *list);
 
+/* 得到列表里面有多少个元素item */
 GIO_AVAILABLE_IN_2_44
 guint                   g_list_model_get_n_items                        (GListModel *list);
 
+/* 得到@position位置的item（也就是获取到数组位置的内容item） */
 GIO_AVAILABLE_IN_2_44
 gpointer                g_list_model_get_item                           (GListModel *list,
                                                                          guint       position);
 
+/* g_list_model_get_item 的返回值加了个类型转化 G_OBJECT (item) */
 GIO_AVAILABLE_IN_2_44
 GObject *               g_list_model_get_object                         (GListModel *list,
                                                                          guint       position);
 
+/* 发射列表改变信号，列表的那个位置发生改变@position，添加了几个，删除了几个 */
 GIO_AVAILABLE_IN_2_44
 void                    g_list_model_items_changed                      (GListModel *list,
                                                                          guint       position,
